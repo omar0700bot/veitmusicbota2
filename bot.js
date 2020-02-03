e43951292e161147857b582947362d5a98454297
omar0700bot <59139063+omar0700bot@users.noreply.github.com>
2020-02-03 13:09:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -352,7 +352,7 @@ client.on('message', message => {
       message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+    client.user.setGame(argresult, "https://www.twitch.tv/veit");
       message.channel.send(`**✅**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
